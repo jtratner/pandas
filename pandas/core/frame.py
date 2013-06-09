c707f164fd82bf9e09b54b739b1f325c67f7645c
Jeffrey Tratner <jeffrey.tratner@gmail.com>
2013-06-09 00:48:17 -0400

    ENH: Add 'mod' method to frame
    NOTE: This is currently *not* accelerated.

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -851,6 +851,7 @@ class DataFrame(NDFrame):
             truediv=True, fill_zeros=np.inf, default_axis=None)
     floordiv = _arith_method(operator.floordiv, 'floor division', '//',
                                  default_axis=None, fill_zeros=np.inf)
+    mod = _arith_method(operator.mod, 'mod', default_axis=None, fill_zeros=np.nan)
     pow = _arith_method(operator.pow, 'pow', '**')
 
     radd = _arith_method(_radd_compat, 'radd')
