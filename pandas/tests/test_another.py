1d739dce781f3082a4494a6ffd30b3fce4cc54e4
Jeffrey Tratner <jeffrey.tratner@gmail.com>
2013-06-11 21:27:34 -0400

    Try moving test to separate file

---
+++ pandas/tests/test_another.py
@@ -0,0 +1,16 @@
+import unittest
+from pandas import DataFrame
+from pandas.core.common import PerformanceWarning
+from pandas.util.testing import assert_produces_warning
+import numpy as np
+
+class RunTest(unittest.TestCase):
+    def test_0000_aa_earlier_test_case(self):
+        # test case prompted by #3788
+        df = DataFrame([[1, np.array([10, 20, 30])],
+                       [1, np.array([40, 50, 60])],
+                       [2, np.array([20, 30, 40])]],
+                       columns=['category', 'arraydata'])
+        grouped = df.groupby('category')
+        with assert_produces_warning(PerformanceWarning):
+            result = grouped.agg(sum)
