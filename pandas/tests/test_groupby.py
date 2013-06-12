d8d2979723ddca1c5c36816f51a8fef11e43c474
Jeffrey Tratner <jeffrey.tratner@gmail.com>
2013-06-11 23:00:49 -0400

    Remove one part and see what happens

--- pandas/tests/test_groupby.py
+++ pandas/tests/test_groupby.py
@@ -346,8 +346,6 @@ class TestGroupBy(unittest.TestCase):
             result = grouped.agg(agg_func)
             expected = Series([agg_func(group) for _, group in grouped],
                               index=index)
-            for r, e in zip(result, expected):
-                assert (r == e).all()
         with assert_produces_warning(PerformanceWarning):
             agg_func = lambda x: x.index[:2]
             result = grouped.agg(agg_func)
