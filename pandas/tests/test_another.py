1ab98dfea62f39b4013127389d4c4470db418678
Jeffrey Tratner <jeffrey.tratner@gmail.com>
2013-06-11 21:52:19 -0400

    Try adding print statements

--- pandas/tests/test_another.py
+++ pandas/tests/test_another.py
@@ -3,6 +3,7 @@ from pandas import DataFrame
 from pandas.core.common import PerformanceWarning
 from pandas.util.testing import assert_produces_warning
 import numpy as np
+import warnings
 
 class RunTest(unittest.TestCase):
     def test_0000_aa_earlier_test_case(self):
@@ -12,5 +13,9 @@ class RunTest(unittest.TestCase):
                        [2, np.array([20, 30, 40])]],
                        columns=['category', 'arraydata'])
         grouped = df.groupby('category')
-        with assert_produces_warning(PerformanceWarning):
+        with assert_produces_warning(PerformanceWarning) as w:
             result = grouped.agg(sum)
+            print result
+            print grouped.agg(sum)
+            print w
+            print warnings.filters
